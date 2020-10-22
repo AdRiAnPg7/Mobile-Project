af301aa5f658fa9e6fd4398f0f56e14b9a277ee4
Adrian <adriancracker@gmail.com>
2020-10-22 16:54:25 -0400

    Añadiendo animacion al splash screen 2

--- app/src/main/java/com/foundmypet/activity_splash_screen.kt
+++ app/src/main/java/com/foundmypet/activity_splash_screen.kt
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
-import kotlinx.android.synthetic.main.activity_splash_screem.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class activity_splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screem)
+        setContentView(R.layout.activity_splash_screen)
         val animation = AnimationUtils.loadAnimation(baseContext, R.anim.animation_rotate)
         imageLogoSplash.startAnimation(animation)
 
