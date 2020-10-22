385f1f111d6b4227770fda642d67aba587d740b3
Adrian <adriancracker@gmail.com>
2020-10-22 13:56:41 -0400

    Add Splash Screem

--- app/src/main/java/com/foundmypet/activity_login.kt
+++ app/src/main/java/com/foundmypet/activity_login.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-class login : AppCompatActivity() {
+class activity_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
