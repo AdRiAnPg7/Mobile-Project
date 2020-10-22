4821923c41d415c6acbcf6379700ac1746934b3d
AugustoDai <53877988+AugustoDai@users.noreply.github.com>
2020-10-22 10:05:39 -0400

    add LoginInteractor

---
+++ app/src/main/java/com/interactor/LoginInteractor.kt
@@ -0,0 +1,22 @@
+package com.foundmypet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val textGoToRegister = findViewById<TextView>(R.id.textRegister)
+
+        textGoToRegister.setOnClickListener{
+            startActivity(Intent(this,register::class.java))
+        }
+
+
+    }
+}
