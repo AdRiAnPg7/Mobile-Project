4c810540e296c60dfef197bf415803743f455d94
Juan Jose <juanjo1998_x@hotmail.com>
2021-01-06 18:49:07 -0400

    post detail added

--- app/src/main/java/com/foundmypet/activities/PostPageActivity.kt
+++ app/src/main/java/com/foundmypet/activities/PostPageActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.foundmypet.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_post_page.*
 
 class PostPageActivity : AppCompatActivity() {
@@ -12,18 +13,36 @@ class PostPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_page)
 
-        val image = BitmapFactory.decodeResource(resources, R.drawable.fotopersonacomun)
+        /*val image = BitmapFactory.decodeResource(resources, R.drawable.fotopersonacomun)
         val round = RoundedBitmapDrawableFactory.create(resources, image)
 
 //        val image2 = BitmapFactory.decodeResource(resources, R.drawable.perro_surf)
 //        val round2 = RoundedBitmapDrawableFactory.create(resources, image)
 
         round.isCircular = true
-        image_user_profile_post_page.setImageDrawable(round)
+        image_user_profile_post_page.setImageDrawable(round)*/
 
 
 //
 //        round2.cornerRadius = 50f
 //        image_user_profile_post_page.setImageDrawable(round2)
+
+        //obtencion de datos de PostListAdapter
+        val postUserName = intent.getStringExtra("iPostUserName")
+        val postDate = intent.getStringExtra("iPostDate")
+        val postDescription = intent.getStringExtra("iPostDescription")
+        val postId = intent.getStringExtra("iPostId")
+        val postImage = intent.getStringExtra("iPostImage")
+        val postUserImage = intent.getStringExtra("iPostUserImage")
+
+
+        text_user_name_post_page.text = postUserName
+        text_time_published_post_page.text = postDate
+        text_description_post_page.text = postDescription
+        Picasso.get().load(postImage).into(image_post_page)
+
+        //imagen usuario
+        Picasso.get().load(postUserImage).into(image_user_profile_post_page)
+
     }
 }
--- app/src/main/java/com/foundmypet/adapters/PostListAdapter.kt
+++ app/src/main/java/com/foundmypet/adapters/PostListAdapter.kt
@@ -1,6 +1,7 @@
 package com.foundmypet
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,7 +20,23 @@ class PostListAdapter(val list: List<Post>): RecyclerView.Adapter<PostListAdapte
             view.descriptionTextView.text = post.postDescription
             //Arreglar lo de abajo, la fecha de antiguedad se debe calcular
             view.commentAntiquityTextView.text = post.postDate
+
+            view.setOnClickListener {
+
+                val intent = Intent(view.context,PostPageActivity::class.java)
+                intent.putExtra("iPostUserName", post.postUserName)
+                intent.putExtra("iPostDate", post.postDate)
+                intent.putExtra("iPostDescription", post.postDescription)
+                intent.putExtra("iPostId", post.postId)
+                intent.putExtra("iPostImage", post.postImage)
+                intent.putExtra("iPostUserImage", post.postUserImage)
+
+                view.context.startActivity(intent)
+            }
+
         }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostListViewHolder {
