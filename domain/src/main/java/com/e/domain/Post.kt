05c550cfe724383423d4fd229c849d68d9d25d72
Gi0vs <53981859+Gi0vs@users.noreply.github.com>
2021-01-09 19:31:01 -0400

    realizado

--- domain/src/main/java/com/e/domain/Post.kt
+++ domain/src/main/java/com/e/domain/Post.kt
@@ -1,4 +1,14 @@
 package com.e.domain
 
 
-data class Post(val postId:String, val postDescription:String, val postImage:String, val postUserImage:String, val postUserName:String, val postDate:String)
+data class Post(val postTittle :String? = null,
+                val postImage :String? = null,
+                val postUserImage:String? = null,
+                val postUserName:String? = null,
+                val postDate:String? = null,
+                val petName:String? = null,
+                val petSpecies:String? = null,
+                val petRace:String? = null,
+                val petColor:String? = null,
+                val phoneNumber:String? = null,
+                val postDescription:String? = null)
