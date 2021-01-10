161e7c69ed7903c8d3dd1315b4e3210ccf60e251
Wilson Ramirez Sandy <wramirez995@gmail.com>
2021-01-10 01:32:43 -0400

    kinda finished with the edit profile

--- app/src/main/java/com/foundmypet/activities/UserProfileActivity.kt
+++ app/src/main/java/com/foundmypet/activities/UserProfileActivity.kt
@@ -1,11 +1,28 @@
 package com.foundmypet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.foundmypet.activities.EditProfileActivity
 
 class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+        val editProfile = findViewById<ImageButton>(R.id.button_edit_profile)
+
+        editProfile.setOnClickListener{
+            startActivity(Intent(this,EditProfileActivity::class.java))
+        }
+
+        val exitProfile = findViewById<ImageButton>(R.id.button_exit_profile)
+
+        exitProfile.setOnClickListener{
+            this.finish()
+        }
+
     }
 }
