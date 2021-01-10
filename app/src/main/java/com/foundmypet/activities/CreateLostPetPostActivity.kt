af874fa654247b25986f5ca95c8289bf1239b6bf
VARGAS VALVERDE <allenbfox@gmail.com>
2021-01-10 19:48:55 -0400

    Primer paso de postear

--- app/src/main/java/com/foundmypet/activities/CreateLostPetPostActivity.kt
+++ app/src/main/java/com/foundmypet/activities/CreateLostPetPostActivity.kt
@@ -1,13 +1,103 @@
 package com.foundmypet
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.activity_create_lost_pet_post.*
 
 class CreateLostPetPostActivity : AppCompatActivity() {
+    private var myUrl = ""
+    private var imageUri:Uri? = null
+    private var storagePostPicRef: StorageReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_lost_pet_post)
-        savePost()
+
+        storagePostPicRef = FirebaseStorage.getInstance().reference.child("Posts Pictures")
+        btn_create_post.setOnClickListener { uploadImage() }
+        CropImage.activity()
+            .setAspectRatio(2,1)
+            .start(this@CreateLostPetPostActivity)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            image_create_lost_pet_post.setImageURI(imageUri)
+        }
+    }
+
+    private fun uploadImage() {
+        when{
+            imageUri == null -> Toast.makeText(this,"Please select image first.",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(text_input_lost_pet_post_tittle.text.toString()) -> Toast.makeText(this, "Please write a title for the Post", Toast.LENGTH_LONG).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Adding New Post")
+                progressDialog.setMessage("Please wait, we are adding your picture post")
+                progressDialog.show()
+
+                val fileRef = storagePostPicRef!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+                var uploadTask:StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+
+                uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                    if (!task.isSuccessful)
+                    {
+                        task.exception?.let {
+                            throw it
+                            progressDialog.dismiss()
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                }).addOnCompleteListener { OnCompleteListener<Uri> { task ->
+                    if (task.isSuccessful) {
+                        val dowloadUrl = task.result
+                        myUrl = dowloadUrl.toString()
+
+                        val ref = FirebaseDatabase.getInstance().reference.child("Posts")
+                        val postId = ref.push().key
+
+                        val postMap = HashMap<String, Any>()
+                        postMap["postId"] = postId!!
+                        postMap["title"] = text_input_lost_pet_post_tittle.text.toString().toLowerCase()
+                        //postMap["publisher"] = FirebaseAuth.getInstance().currentUser!!.uid
+                        postMap["postImage"] = myUrl
+
+                        ref.child(postId).updateChildren(postMap)
+                        Toast.makeText(this,"Post uploaded successfully", Toast.LENGTH_LONG).show()
+                        val intent = Intent (this@CreateLostPetPostActivity, HomePageActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                        progressDialog.dismiss()
+                    }
+                    else{
+                        progressDialog.dismiss()
+                    }
+                } }
+            }
+        }
     }
 
     fun savePost(){
