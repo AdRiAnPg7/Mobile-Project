df1a6c669849f5ff63ded3e72d01adf9d0b1fd67
VARGAS VALVERDE <allenbfox@gmail.com>
2021-01-11 18:27:24 -0400

    Llenando los datos del intent

--- app/src/main/java/com/foundmypet/activities/PostPageActivity.kt
+++ app/src/main/java/com/foundmypet/activities/PostPageActivity.kt
@@ -13,21 +13,7 @@ class PostPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_page)
 
-        /*val image = BitmapFactory.decodeResource(resources, R.drawable.fotopersonacomun)
-        val round = RoundedBitmapDrawableFactory.create(resources, image)
-
-//        val image2 = BitmapFactory.decodeResource(resources, R.drawable.perro_surf)
-//        val round2 = RoundedBitmapDrawableFactory.create(resources, image)
-
-        round.isCircular = true
-        image_user_profile_post_page.setImageDrawable(round)*/
-
-
-//
-//        round2.cornerRadius = 50f
-//        image_user_profile_post_page.setImageDrawable(round2)
-
-        //obtencion de datos de PostListAdapter
+        //DATA INTENT - DATA EXTRA
         val postTittle = intent.getStringExtra("iPostTittle")
         val postImage = intent.getStringExtra("iPostImage")
         val postUserImage = intent.getStringExtra("iPostUserImage")
@@ -40,12 +26,17 @@ class PostPageActivity : AppCompatActivity() {
         val phoneNumber = intent.getStringExtra("iPhoneNumber")
         val postDescription = intent.getStringExtra("iPostDescription")
 
-        text_user_name_post_page.text = postUserName
-        text_time_published_post_page.text = postDate
-        text_description_post_page.text = postDescription
+        text_title_looking_dog.text = postTittle
         Picasso.get().load(postImage).into(image_post_page)
+        text_time_published_post_page.text = postDate
+        text_name_post_page.text = petName
+        text_species_post_page.text = petSpecies
+        text_breed_post_page.text = petRace
+        text_color_post_page.text = petColor
+        text_number_contact_post_page.text = phoneNumber
+        text_description_post_page.text = postDescription
 
-        //imagen usuario
         Picasso.get().load(postUserImage).into(image_user_profile_post_page)
+        text_user_name_post_page.text = postUserName
     }
 }
