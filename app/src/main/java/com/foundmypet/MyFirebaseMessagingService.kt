a0fc3c5303bd26d73d781eca19156bc885061b26
Adrian <adriancracker@gmail.com>
2021-01-12 14:09:35 -0400

    heyy

---
+++ app/src/main/java/com/foundmypet/MyFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.foundmypet
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+}
